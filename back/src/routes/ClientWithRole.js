3efef62bf6f79027ecd4de0b56c33e20a5578595
mlobeto <mercedeslobeto@gmail.com>
2025-01-06 19:13:07 -0300

    client

--- back/src/routes/ClientWithRole.js
+++ back/src/routes/ClientWithRole.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addPropertyToClientWithRole } = require('../controllers');
+const { addPropertyToClientWithRole } = require('../controllers/addPropertyToClientWithRole');
 const router = express.Router();
 
 router.post('/addRole', addPropertyToClientWithRole);
