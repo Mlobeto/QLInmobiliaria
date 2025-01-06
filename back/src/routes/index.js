90149f22fb235de650059d3d32e33154da74afa0
mlobeto <mercedeslobeto@gmail.com>
2025-01-06 18:45:36 -0300

    client

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,14 +1,14 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
 const router = Router();
 
 router.use("/admin", require("./admin"));
-router.use("/auth", require("./auth"))
+router.use("/auth", require("./auth"));
 router.use("/client", require("./client"));
-router.use("/lease", require("./lease"))
+router.use("/lease", require("./lease"));
 router.use("/payment", require("./payment"));
-router.use("/property", require("./property"))
-router.use("/garantor", require("./garantor"))
-router.use("/clientRole", require("./clientWithRole"))
+router.use("/property", require("./property"));
+router.use("/garantor", require("./garantor"));
+router.use("/clientRole", require("./clientWithRole"));
 
 module.exports = router;
