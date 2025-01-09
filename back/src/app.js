7839b3363436e6bade24342de0494e262c2253d4
mlobeto <mercedeslobeto@gmail.com>
2025-01-09 13:52:55 -0300

    cors2

--- back/src/app.js
+++ back/src/app.js
@@ -11,24 +11,10 @@ const allowedOrigins = [
 const app = express()
 
 app.use(express.json())
-const corsOptions = {
-  origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-      } else {
-          callback(new Error("Not allowed by CORS"));
-      }
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(morgan("dev"))
 app.use("/", routes)
 
-app.use((err, req, res, next) => {
-  console.error(err.stack); // Log del error
-  res.status(err.status || 500).json({ error: err.message || "Internal Server Error" });
-});
   
   
 module.exports = app
